2d4f75ceb801654a38633b0e9514f6ee879e9918
awe19ij <71327176+awe19ij@users.noreply.github.com>
2022-11-25 13:10:51 +0900

    Add files via upload
    ...

---
+++ sketch.js
@@ -0,0 +1,11 @@
+let musicFile;
+function setup() {
+
+    // let mysound;
+        soundFormats('mp3', 'ogg');
+        //mysound 
+        musicFile = loadSound('music');
+        musicFile.play(); 
+    }
+
+}
